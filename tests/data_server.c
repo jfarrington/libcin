6b51d172ee49cb5ee8b2dff4549d6ac41b4ef10b
Stuart Wilkins <swilkins@bnl.gov>
2014-01-01 21:52:58 -0500

    Added the data server to serve up images to test

---
+++ tests/data_server.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "data_server.h"
+#include "scrambled_frame.h"
+
+#define MAX_PACKETS 4000
+
+int main(int argc, char *argv[]){
+  udp_packet *packet;
+  int num_packets;
+
+  num_packets = setup_packets(&packet, CIN_PACKET_LEN, 
+                              scrambled_frame, scrambled_frame_len - 312);
+
+  fprintf(stderr, "Frame has %d packets\n", num_packets);
+
+  start_server(packet, num_packets);
+
+  return(0);
+}
+
+int setup_packets(udp_packet **packets, int packet_size, 
+                  unsigned char* stream, unsigned int stream_len){
+  unsigned char* stream_p;
+  int packet_len;
+  int bytes_left;
+  int num_packets = 0;
+  udp_packet *packet_p;
+
+  *packets = (udp_packet *)malloc(MAX_PACKETS * sizeof(udp_packet));
+
+  bytes_left = stream_len;
+  stream_p = stream;
+  packet_p = *packets;
+  while(bytes_left > 0){
+    packet_p->data = malloc(CIN_MAX_MTU * sizeof(unsigned char));
+    if(!packet_p->data){
+      return 0;
+    }
+
+    if((bytes_left - packet_size) >= 0){
+      packet_len = packet_size;
+    } else {
+      packet_len = (unsigned int)bytes_left;
+    }
+
+    packet_p->len = packet_len + 8;
+
+    *((uint64_t*)(packet_p->data)) = 0x0000F4F3F2F1F000;
+    *(packet_p->data) = (unsigned char)num_packets;
+    memcpy((packet_p->data + 8), stream_p, packet_len );
+    stream_p += packet_len;
+    bytes_left -= packet_len;
+    num_packets++;
+    packet_p++;
+  }
+  return num_packets;
+}
+
+int start_server(udp_packet *packets, int num_packets){
+  int s; /* socket */
+  struct sockaddr_in dest_addr;
+  udp_packet* packet_p;
+  uint16_t frame_num;
+  int i = 1;
+  int d;
+
+  fprintf(stderr, "packets = %p\n", packets);
+
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port   = htons(49201);
+  inet_pton(AF_INET, "10.0.5.23", &dest_addr.sin_addr);
+
+  s = socket(AF_INET, SOCK_DGRAM, 0);
+  if(s < 0){
+    perror("Unable to open socket :");
+    return 1;
+  }
+  
+  if(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i)) < 0){
+    perror("setsockopt() failed :");
+    return 1;
+  }
+
+  frame_num = 0;
+
+  while(1){
+    packet_p = packets;
+    for(i=0;i<num_packets;i++){
+      packet_p->data[6] = (char)(frame_num >> 8);
+      packet_p->data[7] = (char)(frame_num & 0xFF);
+      d = sendto(s, packet_p->data, packet_p->len, 0, 
+             (struct sockaddr*) &dest_addr, sizeof(dest_addr));
+      packet_p++;
+    }
+    sleep(1);
+    frame_num++;
+  }
+
+  close(s);
+}
---
+++ tests/data_server.h
@@ -0,0 +1,19 @@
+#ifndef _DATA_SERVER_H 
+#define _DATA_SERVER_H 1
+
+#define CIN_PACKET_LEN          8184
+#define CIN_FRAME_HEIGHT        964
+#define CIN_FRAME_WIDTH         1152
+#define CIN_UDP_DATA_HEADER     8
+#define CIN_FRAME_SIZE          2220744
+#define CIN_MAX_MTU             9000
+
+typedef struct {
+  unsigned char *data;
+  int len;
+} udp_packet;
+
+int setup_packets(udp_packet **packet, int packet_size,unsigned char* stream, unsigned int stream_len);
+int start_server(udp_packet *packets, int num_packets);
+
+#endif
