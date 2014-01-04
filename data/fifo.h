92f9de32926c3ec1b72f18cee2478f327c3b27c5
Stuart Wilkins <swilkins@bnl.gov>
2014-01-04 09:15:11 -0500

    Removed FIFO routines from cindata and placed on own.

---
+++ data/fifo.h
@@ -0,0 +1,26 @@
+#ifndef __CIN_DATA_FIFO__H 
+#define __CIN_DATA_FIFO__H
+
+/* Datastructures */
+
+typedef struct {
+  void *data;
+  void *head;
+  void *tail;
+  void *end;
+  long int size;
+  int elem_size;
+  int full;
+} fifo;
+
+/* FIFO Functions */
+
+void* fifo_get_head(fifo *f);
+void* fifo_get_tail(fifo *f);
+void fifo_advance_head(fifo *f);
+void fifo_advance_tail(fifo *f);
+int fifo_init(fifo *f, int elem_size, long int size);
+long int fifo_used_bytes(fifo *f);
+double fifo_percent_full(fifo *f);
+
+#endif
