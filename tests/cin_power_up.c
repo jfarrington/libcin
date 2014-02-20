234b1b6668ca2a75135e531cba69c302c45ef393
jfarrington <jaimef@sydorinstruments.com>
2014-02-20 17:22:06 -0500

    Added register commands to initialize image stream port

--- tests/cin_power_up.c
+++ tests/cin_power_up.c
@@ -44,6 +44,10 @@ int main(){
 	cin_load_firmware(&cp[0],&cp[1],cin_fpga_config);	
 	sleep(5);
 
+	cin_ctl_write(&cp[0],0x8013,0x057F);
+	usleep(1000);
+	cin_ctl_write(&cp[0],0x8014,0x0A17);
+	usleep(1000);
 	ret_fpga=cin_get_cfg_fpga_status(&cp[0]);
 	sleep(1);
 
